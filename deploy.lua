6d646b3f741e6142819eef706c87de1c1f41b7ea
newObjectccc <897333949@qq.com>
2024-03-19 10:20:07 +0800

    chore: deploy script

---
+++ deploy.lua
@@ -0,0 +1,14 @@
+local os = require("os")
+local repo_path = "~/blog"
+local repo_url = "https://github.com/newObjectccc/newObjectccc.github.io.git"
+
+if os.execute("cd " .. repo_path .. " 2>/dev/null") then
+    -- 如果仓库存在，拉取最新的代码
+    os.execute("cd " .. repo_path .. " && git pull origin main")
+else
+    -- 如果仓库不存在，克隆仓库
+    os.execute("git clone " .. repo_url .. " " .. repo_path)
+end
+
+os.execute("cd " .. repo_path .. " && npm install && npm run docs:build")
+os.execute("cd " .. repo_path .. " && docker-compose up -d")
