073df1eaca90d4121cc080dd10df61576fd8a664
newObjectccc <897333949@qq.com>
2024-03-22 15:17:29 +0800

    chore: docker image build

--- deploy.lua
+++ deploy.lua
@@ -13,4 +13,5 @@ else
 end
 
 os.execute("cd " .. repo_path .. " && npm install && npm run docs:build")
+os.execute("cd  " .. repo_path .. " && docker build -t mycaddy .")
 os.execute("cd " .. repo_path .. " && docker-compose up -d")
